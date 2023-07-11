5a0ae46d48730f4bf36e249ed1cdc6468b265e49
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-07-11 15:30:50 -0500

    add responsive grid

--- src/components/home/ChooseUs.jsx
+++ src/components/home/ChooseUs.jsx
@@ -7,15 +7,41 @@ const ChooseUs = () => {
       <h2 className='text-3xl font-bold mb-5'>
         <span className='bg-black text-white px-2 py-1'>¡Pues te contamos que puedes estar mucho mejor!</span>
       </h2>
-      <p>Aqui te damos 3 razones que <strong>nos hace especiales</strong> como aliados en tecnología</p>
+      <p className='mb-10'>Aqui te damos 3 razones que <strong>nos hace especiales</strong> como aliados en tecnología</p>
+      
+      <ChooseUsGrid className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="grid gap-4">
+              <div>
+                  <div className="h-50 bg-blue-500 max-w-full rounded-lg">
+                    hi
+                  </div>
+              </div>
+              <div>
+                  <div className="h-50 bg-red-500 max-w-full rounded-lg">
+                    hi
+                  </div>
+              </div>
+          </div>
+          <div className="grid gap-4">
+              <div>
+                  <div className="h-50 bg-green-500 max-w-full rounded-lg">
+                    hi
+                  </div>
+              </div>
+          </div>
+      </ChooseUsGrid>
+
     </StyledChooseUs>
   )
 
   return content
 }
 
+
 const StyledChooseUs = styled.div`
-  
+`
+
+const ChooseUsGrid = styled.div`
 `
 
 export default ChooseUs
