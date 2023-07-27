6df973358a85723eaf65b590fb34f11ef04879f6
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-07-26 21:59:55 -0500

    review layout bigger and put the customer name at right

--- src/components/home/Reviews.jsx
+++ src/components/home/Reviews.jsx
@@ -23,17 +23,17 @@ const Reviews = () => {
   };
 
   return (
-    <div className="flex">
-      <button onClick={handlePrev}>Previous</button>
-      <div className="flex flex-wrap">
+    <div className="flex items-center justify-between py-14 space-x-4"> {/* Add space between items */}
+      <button className='bg-gray-300' onClick={handlePrev}>Previous</button>
+      <div className="flex flex-wrap w-full bg-gray-200"> {/* Increase the center gap using space-y */}
         {Object.entries(feedbacks).slice(currentPage * 2, currentPage * 2 + 2).map(([key, value], index) => (
-          <div key={index} className="w-1/2 p-4">
+          <div key={index} className="w-1/2 p-4"> {/* Increase the width to w-3/5 */}
             <p className="bg-blue-200 p-4">{value}</p>
-            <p className="bg-green-200 p-4">{key}</p>
+            <p className="bg-green-200 text-right p-4">{key}</p>
           </div>
         ))}
       </div>
-      <button onClick={handleNext}>Next</button>
+      <button className='bg-gray-300' onClick={handleNext}>Next</button>
     </div>
   );
 }
