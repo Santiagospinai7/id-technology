f3c3b192849d35e45caf0edec732cee017e4f1be
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-07-27 22:54:06 -0500

    footer information logos strusture added

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -1,4 +1,5 @@
-// import footerImg from "../../assets/img/Logo_footer.png";
+import instagramLogo from "../../assets/img/Instagram.png";
+import linkedinLogo from "../../assets/img/LinkedIn.png";
 import footerLogo from "../../assets/img/Logo_footer.png";
 
 const Footer = () => {
@@ -12,15 +13,35 @@ const Footer = () => {
             <h4 className="text-green-medium text-2xl font-semibold mb-8">
               <img src={footerLogo} alt="logo_id" />
             </h4>
-            <ul>
-              <li><a href="/" className="text-black text-lg font-medium">Carrera 67 # 48 - 41. Medellin, Colombia</a></li>
-              <li><a href="/" className="text-black text-lg font-medium">(604)4445616</a></li>
-              <li><a href="/" className="text-black text-lg font-medium">+57 315 9266621</a></li>
-              <li><a href="/" className="text-black text-lg font-medium">info@idtsas.com</a></li>
+            <ul className="space-y-2">
+              <li className="flex items-center space-x-2">
+                <img src={instagramLogo} alt="locationIcon" className="w-6 h-6" />
+                <p className="text-black text-lg font-medium">
+                  Carrera 67 # 48 - 41. Medellin, Colombia
+                </p>
+              </li> 
+              <li className="flex items-center space-x-2">
+                <img src={instagramLogo} alt="telephoneIcon" className="w-6 h-6" />
+                <p className="text-black text-lg font-medium">
+                  (604)4445616
+                </p>
+              </li> 
+              <li className="flex items-center space-x-2">
+                <img src={instagramLogo} alt="whatsAppIcon" className="w-6 h-6" />
+                <p className="text-black text-lg font-medium">
+                  +57 315 9266621
+                </p>
+              </li> 
+              <li className="flex items-center space-x-2">
+                <img src={instagramLogo} alt="emailIcon" className="w-6 h-6" />
+                <p className="text-black text-lg font-medium">
+                  info@idtsas.com
+                </p>
+              </li> 
             </ul>
-            <div className="social-links flex space-x-4">
-              <a href="/"><img src="" alt="instagram_icon" /></a>
-              <a href="/"><img src="" alt="linkedin_icon" /></a>
+            <div className="social-links flex space-x-5 mt-5">
+              <a href="/"><img src={instagramLogo} alt="instagram_icon" /></a>
+              <a href="/"><img src={linkedinLogo} alt="linkedin_icon" /></a>
             </div>
           </div>
         </div>
