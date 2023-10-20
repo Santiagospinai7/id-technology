b5184fc21f0ee4c67cb1fa6d62459078b6fcc103
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-10-19 22:16:17 -0500

    first column of the grid fixed

--- src/components/home/ChooseUs.jsx
+++ src/components/home/ChooseUs.jsx
@@ -55,57 +55,60 @@ const ChooseUs = () => {
         </div>
       </div> */}
 
-      <div className="grid grid-cols-1 md:grid-cols-10 gap-10 mb-20">
-        {/* First Column */}
-        <div className='bg-blue-200 flex flex-col md:col-span-7'>
-          {/* First Row */}
-          <div className='flex h-full'>
-            {/* Left Container */}
-            <div className='bg-blue-300 w-1/3'>
-              <div className="flex justify-center items-center h-full">
-                <img src={chooseUs1} alt="Ecogernos 1" className="max-h-full" />
-              </div>
-            </div>
-            {/* Right Container */}
-            <div className='bg-blue-400 flex flex-col justify-center w-2/3 p-5'>
-              <strong className='text-lg text-green-200 mb-4 text-left'>No trabajamos a ciegas.</strong>
-              <p className='text-sm mb-4 text-left'>Por eso siempre ofrecemos a nuestros clientes <strong>un diagnóstico inicial completamente gratis para identificar sus oportunidades de mejora.</strong> y es que al final cada empresa es un mundo y a nosotros, nos interesa el tuyo.</p>
-            </div>
-          </div>
-
-          {/* Second Row */}
-          <div className='flex h-full'>
-            {/* Left Container */}
-            <div className='bg-blue-400 flex flex-col justify-center w-2/3 p-5'>
-              <strong className='text-lg text-green-200 mb-4 text-right'>No nos limitamos a entregar cajas.</strong>
-              <p className='text-sm mb-4 text-right'>Los dispositivos tecnológicos son herramientas clave las empresas. por eso no solo te acompañamos en el proceso de compra para que elijas el mejor dispositivo de acuerdo a tus necesidades. si no que <strong>te lo instalamos, lo configuramos y te lo dejamos listo para empezar a trabajar.</strong></p>
-              <button className='bg-green-200 text-white px-3 py-2 rounded-3xl mt-4 text-sm'>CONOCE AQUÍ NUESTRO PORTAFOLIO</button>
-            </div>
-            {/* Right Container */}
-            <div className='bg-blue-300 w-1/3'>
-              <div className="flex justify-center items-center h-full">
-                <img src={chooseUs3} alt="Ecogernos 1" className="max-h-full" />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Second Column */}
-        <div className='bg-red-100 col-span-full md:col-span-3'>
-          {/* Top Container */}
-          <div className='bg-blue-300 w-full md:w-auto mb-4 md:mb-0'>
-            <div className="flex justify-center items-center h-full">
-              <img src={chooseUs2} alt="Ecogernos 1" className="max-h-full" />
-            </div>
-          </div>
-          {/* Bottom Container */}
-          <div className='bg-blue-400 flex flex-col justify-center w-full p-5'>
-            <strong className='text-lg text-green-200 mb-4 text-center'>No te dejamos solo.</strong>
-            <p className='text-sm mb-4 text-center'><strong>Una vez decides aliarte con nosotros, cuentas con acompañamiento permanente</strong>, y lo mejor... estamos constantemente atentos a las oportunidades de mejora que surjan en tu empresa. Así que tú tranquilo, nosotros nos encargamos del desarrollo tecnológico de tu compañía.</p>
-            <button className='bg-green-200 text-white px-3 py-2 rounded-3xl mt-4 text-sm'>AGENDA UNA LLAMADA</button>
-          </div>
+<div className="grid grid-cols-1 md:grid-cols-10 gap-10 mb-20">
+  {/* First Column */}
+  <div className='bg-blue-200 flex flex-col md:col-span-7'>
+    {/* First Row */}
+    <div className='flex h-full md:items-end'> {/* Updated to items-end */}
+      {/* Left Container */}
+      <div className='bg-blue-300 w-1/4 md:h-48'> {/* Added h-20 */}
+        <div className="flex justify-center items-center h-full">
+          <div className="bg-cover bg-center w-full h-full" style={{ backgroundImage: `url(${chooseUs1})` }}></div>
         </div>
       </div>
+      {/* Right Container */}
+      <div className='bg-blue-400 flex flex-col justify-center w-3/4 p-5'>
+        <strong className='text-lg text-green-200 mb-4 text-left'>No trabajamos a ciegas.</strong>
+        <p className='text-sm mb-4 text-left'>Por eso siempre ofrecemos a nuestros clientes <strong>un diagnóstico inicial completamente gratis para identificar sus oportunidades de mejora.</strong> y es que al final cada empresa es un mundo y a nosotros, nos interesa el tuyo.</p>
+        <button className='bg-green-200 text-white px-3 py-2 rounded-3xl mt-4 text-sm'>AGENDA TU DIAGNOSTICO GRATUITO</button>
+      </div>
+    </div>
+
+    <div className='my-4'></div>
+
+    {/* Second Row */}
+    <div className='flex h-full md:items-end ml-10'>
+      {/* Left Container */}
+      <div className='bg-blue-400 flex flex-col justify-center w-3/4 p-5'>
+        <strong className='text-lg text-green-200 mb-4 text-right'>No nos limitamos a entregar cajas.</strong>
+        <p className='text-sm mb-4 text-right'>Los dispositivos tecnológicos son herramientas clave las empresas. por eso no solo te acompañamos en el proceso de compra para que elijas el mejor dispositivo de acuerdo a tus necesidades. si no que <strong>te lo instalamos, lo configuramos y te lo dejamos listo para empezar a trabajar.</strong></p>
+        <button className='bg-green-200 text-white px-3 py-2 rounded-3xl mt-4 text-sm'>CONOCE AQUÍ NUESTRO PORTAFOLIO</button>
+      </div>
+      {/* Right Container */}
+      <div className='bg-blue-300 w-1/4 md:h-48'> {/* Added h-20 */}
+        <div className="flex justify-center items-center h-full">
+          <div className="bg-cover bg-center w-full h-full" style={{ backgroundImage: `url(${chooseUs3})` }}></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  {/* Second Column */}
+  <div className='bg-red-100 col-span-full md:col-span-3'>
+    {/* Top Container */}
+    <div className='bg-blue-300 w-full md:w-auto mb-4 md:mb-0'>
+      <div className="flex justify-center items-center h-full">
+        <img src={chooseUs2} alt="Ecogernos 1" className="max-h-20" />
+      </div>
+    </div>
+    {/* Bottom Container */}
+    <div className='bg-blue-400 flex flex-col justify-center w-full p-5'>
+      <strong className='text-lg text-green-200 mb-4 text-center'>No te dejamos solo.</strong>
+      <p className='text-sm mb-4 text-center'><strong>Una vez decides aliarte con nosotros, cuentas con acompañamiento permanente</strong>, y lo mejor... estamos constantemente atentos a las oportunidades de mejora que surjan en tu empresa. Así que tú tranquilo, nosotros nos encargamos del desarrollo tecnológico de tu compañía.</p>
+      <button className='bg-green-200 text-white px-3 py-2 rounded-3xl mt-4 text-sm'>AGENDA UNA LLAMADA</button>
+    </div>
+  </div>
+</div>
 
 
 
