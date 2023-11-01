d9048fa0cbd3de324a15915d3e53b61b5a8f4f8c
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-11-01 17:44:27 -0500

    reviews style

--- src/components/home/Reviews.jsx
+++ src/components/home/Reviews.jsx
@@ -15,8 +15,6 @@ const Reviews = () => {
   const totalPages = Math.ceil(Object.keys(feedbacks).length);
   const totalPages2 = Math.ceil(Object.keys(feedbacks).length / 2);
 
-  console.log('totalPages', totalPages)
-
   const handleNext = () => {
     setCurrentPage((prevPage) => (prevPage + 1) % totalPages);
   };
@@ -53,7 +51,7 @@ const Reviews = () => {
     <div className="text-center py-14 mx-10">
       <h2 className="text-2xl font-bold mb-10">Lo que valoran nuestros clientes de nuestros servicios</h2>
       {windowWidth < 900 && (
-        <div className="flex items-center justify-between py-14 space-x-4 mx-10">
+        <div className="flex items-center justify-center py-14 space-x-4 mx-10">
           <button onClick={handlePrev}>
             <img
               src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAjklEQVR4nO3asQ3CQBAF0WliLdN/JyRImAgCygEhXUSO9HeZ18HXOPDpDiT9yhm4ARvNXYEX8Og+poD7GvMETjRWjglVlglVlglVlgllmVSWSWWZVJZJNarMto7LnzEHjY0YUl+f1k5DjkhhiRSWSGGJFJZIYYkUlkhR/oqHqAklWMfSEdfTlykPBqR/8Abo/nQfGMTYTAAAAABJRU5ErkJggg=="
@@ -63,7 +61,7 @@ const Reviews = () => {
           </button>
           <div className="flex flex-wrap w-full ">
             {Object.entries(feedbacks).slice(currentPage * 1, currentPage * 1 + 1).map(([key, value], index) => (
-              <div key={index} className="w-1/2 p-4">
+              <div key={index} className="w-2/2 p-4">
                 <p className="p-4 text-green-200 font-bold">"{value}"</p>
                 <p className="text-right p-4 font-bold">- {key}</p>
               </div>
