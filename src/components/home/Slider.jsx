c5df50f9789d4f3c98b0757a7ab839627247b654
santiagospinai7 <santiagospidrobo15@gmail.com>
2024-11-14 21:36:49 -0500

    new client slider logos

--- src/components/home/Slider.jsx
+++ src/components/home/Slider.jsx
@@ -1,24 +1,23 @@
-import LogoMicrosoft from "../../assets/img/Partner_Microsoft.png";
+import logos from "../../utils/logos";
 import "../../style/Slider.css";
 
 const Slider = () => {
-  const images = [LogoMicrosoft, LogoMicrosoft, LogoMicrosoft, LogoMicrosoft, LogoMicrosoft];
 
   return (
     <div className="flex items-center justify-center bg-[#f7f7f7]">
       <div className="w-[200%] h-28 overflow-hidden relative flex items-center">
         <div className="w-[550%] sm:w-[300%] md:w-[200%] flex items-center h-20 justify-around absolute left-0 animate gap-20 animate">
-          {images.map((image, index) => {
+          {logos.map((logo, index) => {
             return (
               <div key={index} className="flex justify-center items-start w-[20rem]">
-                <img src={image} alt={`logo-${index}`} />
+                <img src={logo.image} alt={`logo-${logo.alt}`} />
               </div>
             );
           })}
-          {images.map((image, index) => {
+          {logos.map((logo, index) => {
             return (
               <div key={index} className="flex justify-center items-start w-[20rem]">
-                <img src={image} alt={`logo-${index}`} />
+                <img src={logo.image} alt={`logo-${logo.alt}`} />
               </div>
             );
           })}
---
+++ src/utils/logos.js
@@ -0,0 +1,65 @@
+import construsol from '../assets/Recursos/Empresas/Logos-01.png';
+import ingTech from '../assets/Recursos/Empresas/Logos-02.png';
+import certeza from '../assets/Recursos/Empresas/Logos-03.png';
+import dqs from '../assets/Recursos/Empresas/Logos-04.png';
+import dimobe from '../assets/Recursos/Empresas/Logos-05.png';
+import cubiertec from '../assets/Recursos/Empresas/Logos-06.png';
+import axiatech from '../assets/Recursos/Empresas/Logos-07.png';
+import riovivo from '../assets/Recursos/Empresas/Logos-08.png';
+import disFlotas from '../assets/Recursos/Empresas/Logos-09.png';
+import mincivil from '../assets/Recursos/Empresas/Logos-10.png';
+
+const logos = [
+    {
+        id: 1,
+        image: construsol,
+        alt: 'Construsol'
+    },
+    {
+        id: 2,
+        image: ingTech,
+        alt: 'Ing Tech'
+    },
+    {
+        id: 3,
+        image: certeza,
+        alt: 'Certeza'
+    },
+    {
+        id: 4,
+        image: dqs,
+        alt: 'DQS'
+    },
+    {
+        id: 5,
+        image: dimobe,
+        alt: 'Dimobe'
+    },
+    {
+        id: 6,
+        image: cubiertec,
+        alt: 'Cubiertec'
+    },
+    {
+        id: 7,
+        image: axiatech,
+        alt: 'Axiatech'
+    },
+    {
+        id: 8,
+        image: riovivo,
+        alt: 'Riovivo'
+    },
+    {
+        id: 9,
+        image: disFlotas,
+        alt: 'Dis Flotas'
+    },
+    {
+        id: 10,
+        image: mincivil,
+        alt: 'Min Civil'
+    },
+];
+
+export default logos;
